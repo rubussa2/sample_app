9131e17adfc8368eb8b3a5fbed2d84683340522b
rubussa2 <z25048an@aiit.ac.jp>
2025-05-10 06:29:06 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,9 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    # 詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+    session[:session_token] = user.session_token
   end
 
   # 永続的セッションのためにユーザーをデータベースに記憶する
@@ -45,7 +48,7 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
-     forget(current_user)
+    forget(current_user)
     reset_session
     @current_user = nil # 安全のため
   end
