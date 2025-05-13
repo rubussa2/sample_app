1d3f01ab624a3075f90bce335f6c5da113dfad36
rubussa2 <z25048an@aiit.ac.jp>
2025-05-13 12:08:13 +0000

    リスト 13.9:Micropostモデルのバリデーション

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  
+
   def setup
     @user = users(:michael)
     # このコードは慣習的に正しくない
@@ -14,6 +14,16 @@ class MicropostTest < ActiveSupport::TestCase
 
   test "user id should be present" do
     @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
 
