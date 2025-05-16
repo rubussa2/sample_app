844c418b3699bba157b579d5e2adad26700669e3
rubussa2 <z25048an@aiit.ac.jp>
2025-05-16 12:52:05 +0000

    リスト 13.74:画像処理にMiniMagickを利用する設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-   default_scope -> { order(created_at: :desc) }
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
